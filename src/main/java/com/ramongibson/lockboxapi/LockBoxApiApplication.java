aa897f218657b9013b635083f827da2cd93d391b
Ramongibson <ramongibson23@gmail.com>
2024-08-24 08:09:05 -0500

    initial commit

---
+++ src/main/java/com/ramongibson/lockboxapi/LockBoxApiApplication.java
@@ -0,0 +1,13 @@
+package com.ramongibson.lockboxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockBoxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockBoxApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ramongibson/lockboxapi/LockBoxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramongibson.lockboxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockBoxApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
