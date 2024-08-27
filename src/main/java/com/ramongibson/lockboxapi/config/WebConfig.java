85c8756bd89ceb213bf8400a39ea564851028019
Ramongibson <ramongibson23@gmail.com>
2024-08-26 19:36:36 -0500

    Added property for frontend host url

--- src/main/java/com/ramongibson/lockboxapi/config/WebConfig.java
+++ src/main/java/com/ramongibson/lockboxapi/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.ramongibson.lockboxapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    @Value("${lockbox_web_url}")
+    private String lockboxWebUrl;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(lockboxWebUrl)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
             }
         };
